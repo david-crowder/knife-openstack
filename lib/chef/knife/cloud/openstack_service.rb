c17a05c8aa0963e80d213102e2fcaf67863f6ec0
Bobby Ryterski <bobby@ryterski.net>
2015-09-21 08:43:15 -0500

    Support all Fog OpenStack options

--- lib/chef/knife/cloud/openstack_service.rb
+++ lib/chef/knife/cloud/openstack_service.rb
@@ -19,19 +19,7 @@ class Chef
           Chef::Log.debug("openstack_insecure #{Chef::Config[:knife][:openstack_insecure].to_s}")
           Chef::Log.debug("openstack_region #{Chef::Config[:knife][:openstack_region].to_s}")
 
-          super(options.merge({
-                              :auth_params => {
-                                :provider => 'OpenStack',
-                                :openstack_username => Chef::Config[:knife][:openstack_username],
-                                :openstack_api_key => Chef::Config[:knife][:openstack_password],
-                                :openstack_auth_url => Chef::Config[:knife][:openstack_auth_url],
-                                :openstack_endpoint_type => Chef::Config[:knife][:openstack_endpoint_type],
-                                :openstack_tenant => Chef::Config[:knife][:openstack_tenant],
-                                :openstack_region => Chef::Config[:knife][:openstack_region],
-                                :connection_options => {
-                                  :ssl_verify_peer => !Chef::Config[:knife][:openstack_insecure]
-                                }
-                }}))
+          super(options.merge(auth_params: get_auth_params))
         end
 
         # add alternate user defined api_endpoint value.
@@ -57,6 +45,22 @@ class Chef
           rescue Excon::Errors::BadRequest => e
             handle_excon_exception(CloudExceptions::KnifeCloudError, e)
           end
+        end
+
+        def get_auth_params
+          params = {
+            provider: 'OpenStack',
+            connection_options: {
+              ssl_verify_peer: !Chef::Config[:knife][:openstack_insecure]
+            }
+          }
+
+          Fog::Compute::OpenStack.recognized.each do |k|
+            next unless k.to_s.start_with?('openstack')
+            params[k] = Chef::Config[:knife][k]
+          end
+
+          params
         end
       end
     end
